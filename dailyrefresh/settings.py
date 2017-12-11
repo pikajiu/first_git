bf45251463059580521ece13dd90ec9a6d9b0ca1
Yuan Luo <871698795@qq.com>
2017-12-11 23:36:13 +0800

    add admin,but exception occure : Non-relational field given in select_related: id. Choices are: gtype when change in admin

--- dailyrefresh/settings.py
+++ dailyrefresh/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tinymce',
     'df_user',
     'df_goods',
 ]

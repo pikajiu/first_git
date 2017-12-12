a857d8feff82cb73cc6520aba2da60023c50d0b1
Yuan Luo <871698795@qq.com>
2017-12-13 00:46:47 +0800

    when comment there modelAdmin class , the exception : Non-relational field given in select_related in admin operation disappear. so I suppose that the version I use is 1.11 and after Django 1.8 added validation for non-relational fields in select_related().please confirm later.

--- df_goods/admin.py
+++ df_goods/admin.py
@@ -3,18 +3,19 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
 from df_goods.models import TypeInfo, GoodsInfo
 
 
-class TypeInfoAdmin(admin.ModelAdmin):
-    list_select_related = ['id', 'ttitle']
+# class TypeInfoAdmin(admin.ModelAdmin):
+#     list_select_related = ['id', 'ttitle']
 
 
-class GoodsInfoAdmin(admin.ModelAdmin):
-    list_per_page = 15
-    list_select_related = ['id', 'gtitle', 'gprice', 'gunit', 'gclick', 'gkucun', 'gcontent', 'gtype']
+# class GoodsInfoAdmin(admin.ModelAdmin):
+#     list_per_page = 15
+#     list_select_related = ['id', 'gtitle', 'gprice', 'gunit', 'gclick', 'gkucun', 'gcontent', 'gtype']
 
 
-admin.site.register(TypeInfo, TypeInfoAdmin)
-admin.site.register(GoodsInfo, GoodsInfoAdmin)
+# admin.site.register(TypeInfo, TypeInfoAdmin)
+# admin.site.register(GoodsInfo, GoodsInfoAdmin)
+admin.site.register(TypeInfo)
+admin.site.register(GoodsInfo)

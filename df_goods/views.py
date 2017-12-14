881d9f80e9d2a8f59cf65806085100b5f6d4de03
Yuan Luo <871698795@qq.com>
2017-12-15 00:29:58 +0800

    add index logic!

--- df_goods/views.py
+++ df_goods/views.py
@@ -1,6 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from df_goods.models import TypeInfo, GoodsInfo
+
+
+def index(request):
+    typelist = TypeInfo.objects.all()
+    type00 = typelist[0].goodsinfo_set.order_by('-id')[0:4]
+    type01 = typelist[0].goodsinfo_set.order_by('-gclick')[0:4]
+    type10 = typelist[0].goodsinfo_set.order_by('-id')[0:4]
+    type11 = typelist[0].goodsinfo_set.order_by('-gclick')[0:4]
+    type20 = typelist[0].goodsinfo_set.order_by('-id')[0:4]
+    type21 = typelist[0].goodsinfo_set.order_by('-gclick')[0:4]
+    type30 = typelist[0].goodsinfo_set.order_by('-id')[0:4]
+    type31 = typelist[0].goodsinfo_set.order_by('-gclick')[0:4]
+    type40 = typelist[0].goodsinfo_set.order_by('-id')[0:4]
+    type41 = typelist[0].goodsinfo_set.order_by('-gclick')[0:4]
+    type50 = typelist[0].goodsinfo_set.order_by('-id')[0:4]
+    type51 = typelist[0].goodsinfo_set.order_by('-gclick')[0:4]
+    context = {'type00': type00,
+               'type01': type01,
+               'type10': type10,
+               'type11': type11,
+               'type20': type20,
+               'type21': type21,
+               'type30': type30,
+               'type31': type31,
+               'type40': type40,
+               'type41': type41,
+               'type50': type50,
+               'type51': type51,
+               }
+    return render(request,'df_goods/index.html',context)
--- df_user/urls.py
+++ df_user/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 from . import views
+
 urlpatterns = [
-    url(r'^register/$',views.register),
-    url(r'^register_handle/$',views.register_handle),
-    url(r'^register_exist/$',views.register_exist),
-    url(r'^login/$',views.login),
+    url(r'^register/$', views.register),
+    url(r'^register_handle/$', views.register_handle),
+    url(r'^register_exist/$', views.register_exist),
+    url(r'^login/$', views.login),
     url(r'^login_handle/$', views.login_handle),
-    url(r'^info/$',views.user_info),
-    url(r'^order/$',views.user_order),
-    url(r'^site/$',views.user_site),
+    url(r'^info/$', views.user_info),
+    url(r'^order/$', views.user_order),
+    url(r'^site/$', views.user_site),
 ]
